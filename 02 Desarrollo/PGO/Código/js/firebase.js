4b886003663cdeb99e505d90ac5e20a733e47a84
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2022-11-28 22:03:27 -0500

    Agrega configuración inicial del proyecto de Firebase: profe-go Ítem agregado: 	- js/firebase.js

---
+++ 02 Desarrollo/PGO/Código/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMxOYTKtj2Dg8bUiJXdqQRrrIpu-4-65M",
+  authDomain: "profe-go.firebaseapp.com",
+  projectId: "profe-go",
+  storageBucket: "profe-go.appspot.com",
+  messagingSenderId: "612714822034",
+  appId: "1:612714822034:web:2398c2b4cf990838b1a94b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
