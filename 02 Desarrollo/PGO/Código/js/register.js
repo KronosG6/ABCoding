a6cc0d15e063f1e83667cfe025ad1c738d9c39ff
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2022-11-30 13:42:33 -0500

    Agrega funciones para guardar datos en Firestore

--- 02 Desarrollo/PGO/Código/js/register.js
+++ 02 Desarrollo/PGO/Código/js/register.js
@@ -1,15 +1,19 @@
 // TODO: Integrar mensajes tipo toast
 //! Validar contraseñas
 //* Redireccionar a página principal tras registro exitoso
-//* Agregar datos del usuario en Firestore
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import {
+  doc,
+  setDoc,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 
 const registerForm = document.querySelector("#register-form");
 
 registerForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
+  const name = registerForm["nombre-floating-input"].value;
   const email = registerForm["correo-floating-input"].value;
   const password = registerForm["passwd-floating-input"].value;
 
@@ -19,7 +23,11 @@ registerForm.addEventListener("submit", async (e) => {
       email,
       password
     );
-    console.log(`Bienvenido ${userCredentials.user.email}`);
+
+    const docRef = await setDoc(doc(db, "usuario", userCredentials.user.uid), {
+      nombre: name
+    });
+    console.log(`Bienvenid@ ${name}`);
   } catch (error) {
     if (error.code === "auth/email-already-in-use") {
       console.log("Correo ya está en uso");
