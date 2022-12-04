91eafb6de63ed7a867187c4eb2fd20617f1fb3b8
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2022-12-04 15:02:52 -0500

    feat: :sparkles: Agrega funcionalidad de login y registro con Google

--- 02 Desarrollo/PGO/Código/js/login.js
+++ 02 Desarrollo/PGO/Código/js/login.js
@@ -1,34 +1,67 @@
 //* Redireccionar a página principal tras registro exitoso
 //* Agregar funcionalidad al checkbox (Recuérdame)
 //* Agregar funcionalidad al link (Olvidó su contraseña)
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
 } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import {
+  doc,
+  setDoc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 
 const loginForm = document.querySelector("#login-form");
 const passwdBtn = document.querySelector("#passwd-btn");
 const passwdInput = document.querySelector("#passwd-floating-input");
 
+const googleBtn = document.querySelector("#btn-google");
+
 loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-
   const email = loginForm["correo-floating-input"].value;
   const password = loginForm["passwd-floating-input"].value;
-
   try {
     const userCredentials = await signInWithEmailAndPassword(
       auth,
       email,
       password
     );
-    showToast(`Bienvenido ${userCredentials.user.email}`);
+    const docRef = doc(db, "usuario", userCredentials.user.uid);
+    const docSnap = await getDoc(docRef);
+    showToast(`Bienvenido ${docSnap.data().nombre}`);
   } catch (error) {
     if (error.code === "auth/user-not-found") {
       showToast("Correo no encontrado");
     } else if (error.code === "auth/wrong-password") {
       showToast("Contraseña incorrecta");
+    } else if (error.code) {
+      showToast("Algo salió mal");
+    }
+  }
+});
+
+googleBtn.addEventListener("click", async () => {
+  const googleProvider = new GoogleAuthProvider();
+  try {
+    const userCredentials = await signInWithPopup(auth, googleProvider);
+    await setDoc(doc(db, "usuario", userCredentials.user.uid), {
+      nombre: userCredentials.user.displayName,
+    });
+    showToast(`Bienvenido ${userCredentials.user.displayName}`);
+  } catch (error) {
+    if (error.code === "auth/popup-closed-by-user") {
+      showToast("Operación cancelada");
+    } else if (error.code === "auth/account-exists-with-different-credential") {
+      showToast("Ya existe una cuenta con este correo");
+    } else if (error.code === "auth/cancelled-popup-request") {
+      showToast("El navegador impidió abrir un Pop-Up");
+    } else if (error.code === "auth/unauthorized-domain") {
+      showToast("Dominio no autorizado");
+      console.log("Utilizar localhost en lugar de 127.0.0.1");
     } else if (error.code) {
       showToast("Algo salió mal");
     }
--- 02 Desarrollo/PGO/Código/js/register.js
+++ 02 Desarrollo/PGO/Código/js/register.js
@@ -1,7 +1,11 @@
 //* Implementar 2 tipos de mensajes: {error, success}
 //* Redireccionar a página principal tras registro exitoso
 import { auth, db } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 import {
   doc,
   setDoc,
@@ -13,27 +17,25 @@ const passwdInput = document.querySelector("#passwd-floating-input");
 const repPasswdBtn = document.querySelector("#rep-passwd-btn");
 const repPasswdInput = document.querySelector("#rep-passwd-floating-input");
 
+const googleBtn = document.querySelector("#btn-google");
+
 registerForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-
   const name = registerForm["nombre-floating-input"].value;
   const email = registerForm["correo-floating-input"].value;
   const password = registerForm["passwd-floating-input"].value;
   const repPassword = registerForm["rep-passwd-floating-input"].value;
-
   try {
     // también se puede validar seguridad de la contraseña
     if (password !== repPassword) {
       throw "diffPasswd";
     }
-
     const userCredentials = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
-
-    const docRef = await setDoc(doc(db, "usuario", userCredentials.user.uid), {
+    await setDoc(doc(db, "usuario", userCredentials.user.uid), {
       nombre: name,
     });
     showToast(`Bienvenido ${name}`);
@@ -46,6 +48,30 @@ registerForm.addEventListener("submit", async (e) => {
       showToast("Correo inválido");
     } else if (error.code === "auth/weak-password") {
       showToast("Contraseña demasiado débil");
+    } else if (error.code) {
+      showToast("Algo salió mal");
+    }
+  }
+});
+
+googleBtn.addEventListener("click", async () => {
+  const googleProvider = new GoogleAuthProvider();
+  try {
+    const userCredentials = await signInWithPopup(auth, googleProvider);
+    await setDoc(doc(db, "usuario", userCredentials.user.uid), {
+      nombre: userCredentials.user.displayName,
+    });
+    showToast(`Bienvenido ${userCredentials.user.displayName}`);
+  } catch (error) {
+    if (error.code === "auth/popup-closed-by-user") {
+      showToast("Operación cancelada");
+    } else if (error.code === "auth/account-exists-with-different-credential") {
+      showToast("Ya existe una cuenta con este correo");
+    } else if (error.code === "auth/cancelled-popup-request") {
+      showToast("El navegador impidió abrir un Pop-Up");
+    } else if (error.code === "auth/unauthorized-domain") {
+      showToast("Dominio no autorizado");
+      console.log("Utilizar localhost en lugar de 127.0.0.1");
     } else if (error.code) {
       showToast("Algo salió mal");
     }
