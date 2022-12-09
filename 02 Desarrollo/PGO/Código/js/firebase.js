719ff1231bcf08529288315e78089a46b3b248f0
Gibran <gibrantarrillo1604@gmail.com>
2022-12-09 10:26:20 -0500

    cambios de prueba contactos

--- 02 Desarrollo/PGO/Código/js/firebase.js
+++ 02 Desarrollo/PGO/Código/js/firebase.js
@@ -3,6 +3,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebas
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+/*import {getDatabase} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";*/
+
 // SDK's agregados - CDN de Firebase
 // Autenticación
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
@@ -22,3 +24,7 @@ export const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+
+/*//Initialize Firebase Realtime Database and get a reference to the service
+export const database = getDatabase(app);*/
