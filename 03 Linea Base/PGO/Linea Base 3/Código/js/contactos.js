968890c73bc9237ff0d8606aed90343695e6c55c
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2022-12-12 00:07:29 -0500

    Establecer Línea Base 3 del proyecto PGO.

---
+++ 03 Linea Base/PGO/Linea Base 3/Código/js/contactos.js
@@ -0,0 +1,180 @@
+import { auth, db } from "./firebase.js";
+import {
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  collection,
+  onSnapshot,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
+var userUID;
+var unsuscribe;
+var activeChat = "";
+const contenedorContactos = document.getElementById("contactos");
+const contenedorMensajes = document.getElementById("bodyContent");
+const contactoSeleccionado = document.getElementById("selected-contact");
+const botonEnviar = document.getElementById("submit");
+const mensajeTextArea = document.getElementById("message");
+
+window.addEventListener("load", async () => {
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      // User logged in already or has just logged in.
+      userUID = user.uid;
+      await getContactos(user.uid);
+      mensajeTextArea.value = "";
+    } else {
+      // User not logged in or has just logged out.
+      console.log("El usuario no ha iniciado sesión");
+    }
+  });
+});
+
+botonEnviar.addEventListener("click", async () => {
+  const mensaje = mensajeTextArea.value;
+  if (mensaje != "") {
+    mensajeTextArea.value = "";
+    const tiempo = new Date();
+    await setDoc(doc(db, `chat/${activeChat}/mensajes`, tiempo.toISOString()), {
+      mensaje,
+      creado_por: userUID,
+    });
+  }
+});
+
+mensajeTextArea.addEventListener("keyup", (event) => {
+  event.target.value = event.target.value.replace(/[\r\n\v]+/g, "");
+  if (event.key == "Enter") {
+    botonEnviar.click();
+  }
+});
+
+/**
+ * Obtiene los contactos del usuario
+ *
+ * @param {String} userId - el del usuario
+ */
+async function getContactos(userId) {
+  try {
+    const conexiones = await getDocs(
+      collection(db, `usuario/${userId}/conexion`)
+    );
+    conexiones.forEach(async (contacto) => {
+      await createHTMLContacto(contacto.id, contacto.data().id_chat);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+/**
+ * Obtiene los mensajes de la base de datos y luego los muestra en la pantalla.
+ * @param {String} idChat - la identificación del chat
+ */
+async function getMensajes(idChat, nombreChat, urlFoto) {
+  try {
+    const activeChatDiv = `
+      <div class="avatar">
+        <img src="${
+          urlFoto === "default" ? "../img/user-circle.svg" : urlFoto
+        }" alt="perfil">
+      </div>
+      <div class="info-user-selected">
+        <span class="name-user">${nombreChat}</span>
+      </div>
+    `;
+    contactoSeleccionado.innerHTML = activeChatDiv;
+    unsuscribe = onSnapshot(
+      collection(db, `chat/${idChat}/mensajes`),
+      (snapshot) => {
+        contenedorMensajes.innerHTML = "";
+        snapshot.docs.forEach(async (msg) => {
+          showMensaje(msg.data().mensaje, msg.data().creado_por == userUID);
+        });
+        contenedorMensajes.scrollTop = contenedorMensajes.scrollHeight;
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+/**
+ * Muestra información del contacto
+ *
+ * @param {String} idContacto - el id del usuario que desea mostrar
+ */
+async function createHTMLContacto(idContacto, idChat) {
+  try {
+    const docRef = doc(db, "usuario", idContacto);
+    const docSnap = await getDoc(docRef);
+    const nombre = docSnap.data().nombre;
+    const urlFoto = docSnap.data().url_foto;
+    const contactDiv = `
+        <div id="${idChat}" class="user">
+            <div class="avatar">
+            <img src="${
+              urlFoto === "default" ? "../img/user-circle.svg" : urlFoto
+            }" alt="perfil">
+            </div>
+            <div class="info-user">
+                <span class="name-user">${nombre}</span>
+            </div>
+        </div>
+    `;
+    contenedorContactos.innerHTML += contactDiv;
+
+    // Se agrega un listener onclick para cada contacto
+    const listaContactos = document.querySelectorAll(".user");
+    listaContactos.forEach((item) => {
+      item.addEventListener("click", async function () {
+        activeChat = item.id;
+        mensajeTextArea.value = "";
+        await getMensajes(
+          item.id,
+          item.querySelector("span").innerText,
+          item.querySelector("img").src
+        );
+        document.getElementById("portada-contacto").style.display = "none";
+        document.getElementById("box-chat").style.display = "block";
+      });
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+/**
+ * Muestra un mensaje segun el emisor
+ *
+ * @param {String} mensaje - el mensaje a mostrar
+ * @param {Boolean} tipo - true, para el usuario actual | false, para el otro
+ */
+function showMensaje(mensaje, tipo) {
+  if (tipo) {
+    const boxDiv = `
+        <div class="box-right">
+            <div class="space"></div>
+            <div class="message m-right">
+                <p>${mensaje}</p>
+            </div>
+        </div>
+    `;
+    contenedorMensajes.innerHTML += boxDiv;
+  } else {
+    const boxDiv = `
+        <div class="box-left">
+            <div class="message m-left">
+                <p>${mensaje}</p>
+            </div>
+            <div class="space"></div>
+        </div>
+    `;
+    contenedorMensajes.innerHTML += boxDiv;
+  }
+}
---
+++ 03 Linea Base/PGO/Linea Base 3/Código/js/firebase.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+/*import {getDatabase} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";*/
+
+// SDK's agregados - CDN de Firebase
+// Autenticación
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+// Firestore
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+// Storage
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMxOYTKtj2Dg8bUiJXdqQRrrIpu-4-65M",
+  authDomain: "profe-go.firebaseapp.com",
+  projectId: "profe-go",
+  storageBucket: "profe-go.appspot.com",
+  messagingSenderId: "612714822034",
+  appId: "1:612714822034:web:2398c2b4cf990838b1a94b",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);
+
+
+/*//Initialize Firebase Realtime Database and get a reference to the service
+export const database = getDatabase(app);*/
+
